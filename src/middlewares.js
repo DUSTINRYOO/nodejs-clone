1b224f06b4c43d1c81e8c281277dff12a11112be
DUSTINRYOO <hyeony77@gmail.com>
2023-01-01 15:59:01 -0500

    Course up to User Authentication
    Github login
    Token / API / Session etc

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,6 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session;
   res.locals.siteName = "Wetube";
-  console.log(res.locals);
   next();
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { see, logout, edit, remove } from "../controllers/userController";
+import {
+  startGithubLogin,
+  finishGithubLogin,
+  see,
+  logout,
+  edit,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
 userRouter.get("/:id", see);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
