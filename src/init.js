824383832f155e0923f72f1a7b234c6f4df5ff46
DUSTINRYOO <hyeony77@gmail.com>
2023-01-08 22:35:53 -0500

    Preparing Deployment

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/user";
 import "./models/comment";
 import app from "./server";
 
-const PORT = 8000;
+const PORT = 4000;
 
 const handleListening = () => console.log("Server is working");
 app.listen(PORT, handleListening);
